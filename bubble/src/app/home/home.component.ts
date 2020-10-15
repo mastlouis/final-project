b852f991f63bf3dd6bda7f1f9408d7fbc69b1cf4
streetsandstars <ahjicha@gmail.com>
2020-10-15 14:45:23 -0400

    tutorial

--- bubble/src/app/home/home.component.ts
+++ bubble/src/app/home/home.component.ts
@@ -23,67 +23,64 @@ export class HomeComponent implements OnInit {
     let Peer = new SimplePeer;
     let ws, msgs = []
       let p = null;
- 
-      const socketReady = new Promise( (resolve, reject) => {
-        ws = new WebSocket(`ws://127.0.0.1:4200`);
- 
-        ws.onopen = () => {
- 
-          ws.onmessage = __msg => {
-            const msg = JSON.parse( __msg.data )
-            if( msg.address === 'connect' )  {
-              resolve( msg.initiator )
-            }else{
-              p.signal( msg );
-            }
-          };
-        };
- 
-    })
- 
-    console.log("made it here");
-    const makeConnection = function(initiator) {
-      console.log("making connection");
-      ws.send( initiator );
-    };
- 
-     // get video/voice stream as Promise
-     const avReady = navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    });
- 
-      Promise.all( [avReady, socketReady] ).then( values => {
-        const stream = values[0];
- 
-        p = new Peer({
-          initiator: values[1],
-          trickle: false,
-          stream
-        });
- 
-        p.on("signal", data => {
-          makeConnection( JSON.stringify(data) );
-        });
- 
-        p.on("stream", stream => {
-          // got remote video stream, now let's show it in a video tag
-          var video = document.querySelector("#their-video") as HTMLVideoElement;
- 
-          if ("srcObject" in video) {
-            video.srcObject = stream;
-          } else {
-            // video. = window.URL.createObjectURL(stream);
-          }
- 
-          console.log("made it here")
-          video.play()
-        })
-      })
-    }
-    
 
-      
+  const socketReady = new Promise( (resolve, reject) => {
+    ws = new WebSocket(`ws://${window.location.host}`);
+
+    ws.onopen = () => {
+
+      ws.onmessage = __msg => {
+        const msg = JSON.parse( __msg.data )
+        if( msg.address === 'connect' )  {
+          resolve( msg.initiator )
+        }else{
+          p.signal( msg );
+        }
+      };
+    };
+  });
+
+  // get video/voice stream as Promise
+  const avReady = navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  });
+
+  const makeConnection = function(initiator) {
+    console.log("making connection");
+    ws.send( initiator );
+  };
+
+  Promise.all( [avReady, socketReady] ).then( values => {
+    const stream = values[0];
+
+    p = new Peer({
+      initiator: values[1],
+      trickle: false,
+      stream
+    });
+
+    p.on("signal", data => {
+      makeConnection( JSON.stringify(data) );
+    });
+
+    p.on("stream", stream => {
+      // got remote video stream, now let's show it in a video tag
+      var video = document.querySelector("#their-video") as HTMLVideoElement;
+
+      if ("srcObject" in video) {
+        video.srcObject = stream;
+      } else {
+        // video.src = window.URL.createObjectURL(stream);
+        console.log("reached unreachable")
+      }
+
+      video.play()
+    })
+    
+  })}
+
+    
   toggleAudio() {
     if(this.audio) {
       this._rtc.removeSelfAudio();
