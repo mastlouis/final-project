889f9deba9613bf8a9306575d214d453fe74cd28
streetsandstars <ahjicha@gmail.com>
2020-10-15 13:07:30 -0400

    server?

--- server.js
+++ server.js
@@ -17,6 +17,42 @@ client.connect(err => {
 
 app.use(express.static(path.join(__dirname, "bubble/dist/bubble")));
 
+/*start of changes*/
+const server = http.createServer( app )
+
+const socketServer = new ws.Server({ server })
+
+const clients = []
+
+let count = 0
+socketServer.on( 'connection', client => {
+  // when the server receives a message from this client...
+  client.on( 'message', msg => {
+	  // send msg to every client EXCEPT
+    // the one who originally sent it. in this demo this is
+    // used to send p2p offer/answer signals between clients
+    clients.forEach( c => {
+      if( c !== client )
+        c.send( msg )
+    })
+  })
+
+  // add client to client list
+  clients.push( client )
+  
+  client.send(
+    JSON.stringify({ 
+      address:'connect',
+      // we only initiate p2p if this is the second client connected
+      initiator:++count % 2 === 0
+    })
+  )
+  
+})
+
+
+/* end of changes */
+
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/bubble/dist/bubble/index.html");
