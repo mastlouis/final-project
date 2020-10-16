eaf9f3c2aefa097bedbde06b2de2c5e3b0cdc48c
mastlouis <mastlouis@wpi.edu>
2020-10-15 17:51:51 -0700

    ONE LINE FIX. I'M MAD

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ socketServer.on( 'connection', client => {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = server.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
